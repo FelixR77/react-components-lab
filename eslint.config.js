050c17848b3eb00fd802a61ac19399fdf8058753
FelixR77 <felix.romero75@gmail.com>
2025-11-05 10:20:28 -0600

    init commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off', // add this line - added by instructor - 
+      //  the first prevents warnings if you dont declare types for your props, which we are not in this lab. 
+      //  the second, prevents warnings if you use contractions in JSX
+      'react/no-unescaped-entities': 'off', // add this line
+
+    },
+  },
+])
